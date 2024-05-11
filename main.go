b2c7116cd96df07b6c8d1923ede92d82e1abb2b6
Obsidian <obsidiancrystal@tutanota.com>
2024-05-11 12:54:53 +0530

    modified:   main.go

--- main.go
+++ main.go
@@ -4,20 +4,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 	"github.com/gorilla/mux"
 )
 
-func ServeHTTP (w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w,"Welcome to port %d!\n",3000);
+type book struct {
+	id           int;
+	title        string;
+	author       string;
+	ISBN         int;
+	PublishDate  Time.Now();
 }
 
+func getBlockchain(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"Welcome to port %d!\n",3000);
+}
 
 func main() {
 	fmt.Println("Hello World!");
 
 	r := mux.NewRouter();
-	r.HandleFunc("/", ServeHTTP).Methods("GET");
-	r.HandleFunc("/",).Methods("POST");
+	r.HandleFunc("/", getBlockchain).Methods("GET");
+	r.HandleFunc("/", writeBlock).Methods("POST");
 	r.HandleFunc("/new", newBook).Methods("POST");
 
 	http.ListenAndServe(":3000",r);
