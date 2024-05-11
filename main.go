129322258cd3cfd5fab470e2ff348b4227c1e479
Obsidian <obsidiancrystal@tutanota.com>
2024-05-11 12:23:24 +0530

    modified:   main.go

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/gorilla/mux"
 )
 
+func ServeHTTP (w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"Welcome to port %s!\n", r.URL.Path);
+}
 
 
+func main() {
+	fmt.Println("Hello World!");
 
+	r := mux.NewRouter();
+	r.HandleFunc("/", ServeHTTP).Methods("GET");
 
-
+	http.ListenAndServe(":3000",r);
+}
 
 
